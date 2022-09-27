61244587d8240822d5e3fc3cbbf0dd8c034ffd04
Achrafcodes <essoussygamer@gmail.com>
2022-09-27 13:27:19 +0100

    starter

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,25 @@
 "use strict";
 
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  friday: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
+// Data needed for first part of the section
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
@@ -23,5 +43,4 @@ console.log(rest.get(arr));
 console.log(rest);
 console.log(Object.entries(openingHours));
 const mapHours = new Map(Object.entries(openingHours));
-const question = new Map(["question", "what is the best programming language"]);
-console.log(rest);
+console.log(mapHours);
